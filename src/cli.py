ec9905346330b0a2c1ed1c0f160f808180c99f93
Fred Bliss <fbliss@gmail.com>
2025-06-22 13:59:12 -0500

    fix

--- src/cli.py
+++ src/cli.py
@@ -39,9 +39,8 @@ def main():
     )
     parser.add_argument(
         "--enhancer_model_name",
-        # default="speechbrain/sepformer-dns4-16k-enhancement",
-        default="htdemucs_ft.yaml"
-        help="Model name for Stage 2 speech enhancement (from SpeechBrain/Hugging Face)."
+        default="speechbrain/sepformer-dns4-16k-enhancement",
+        help="Model name for Stage 2 speech enhancement (from SpeechBrain/Hugging Face).",
     )
     parser.add_argument(
         "--video",
